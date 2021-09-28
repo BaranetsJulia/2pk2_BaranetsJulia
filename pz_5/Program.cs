e1666b69e1a273dcc3778fe12e3fcc62444c3be5
Юлия Ивановна Баранец <baranets.yi1251@oksei.onmicrosoft.com>
2021-09-28 15:01:19 +0500

    init

--- pz_5/Program.cs
+++ pz_5/Program.cs
@@ -6,7 +6,36 @@ namespace pz_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Задание 1");
+            for (int i = 0; i <= 80; i += 4)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Задание 3");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("####");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Задание 4");
+            for (int j = 0; j <= 100; j++)
+            {
+                if (j % 5 == 0)
+                {
+                    Console.WriteLine(j + " ");
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Задание 5");
+            for (int i = 1, j = 40; i - j != 39; i++, j--)
+            {
+                Console.WriteLine(i + " " + j);
+            }
+            Console.ReadLine();
         }
     }
 }
