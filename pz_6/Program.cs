0fa38a44e438d939aa64eb17534bc0ab5d98cfbb
Юлия Ивановна Баранец <baranets.yi1251@oksei.onmicrosoft.com>
2021-09-29 14:38:35 +0500

    init

---
+++ pz_6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace pz_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.Write("Введите число ");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = 1;
+            c = a % b;
+            while (c == 0)
+            {
+                b++;
+                if (a % b == 0)
+                {
+                    Console.WriteLine(b);
+                    break;
+                }
+            }
+            
+            
+              
+        }
+    }
+}
