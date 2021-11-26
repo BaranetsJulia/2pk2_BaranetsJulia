a269d7df843bab5d5999f5f3c99af90cd5cd4c26
Юлия Ивановна Баранец <baranets.yi1251@oksei.onmicrosoft.com>
2021-11-26 22:55:50 +0500

    init

---
+++ pz_16/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace pz_16
+{
+    class Program
+    {
+        //Вариант 3
+        static void GetLimetSumm(int n, out int a)
+        {
+            a = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                a += i;
+            }
+            Console.WriteLine(a);
+        }
+        static void Main(string[] args)
+        {
+            int sum;
+            Console.WriteLine("Введите число: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            GetLimetSumm(n, out sum);
+        }
+    }
+
+}
