1e1bfe1ee9a04e0c48df758c680daba1601a66f3
Юлия Ивановна Баранец <baranets.yi1251@oksei.onmicrosoft.com>
2021-12-08 22:54:16 +0500

    init

---
+++ pz_18(2)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace pz_18_2_
+{
+    class Program
+    {
+        //Вариант 3
+        //Задание 2
+        static double Geometr(int n, double b, double q)
+        {
+            if (n == 1) return b;
+            else return Geometr(n - 1, b, q) * q;
+        }
+        static void Main(string[] args)
+        {
+            double q = -0.5;
+            double b = 3;
+            int n = Convert.ToInt32(Console.ReadLine());
+            double x = Geometr(n, b, q);
+            Console.WriteLine(x);
+        }
+    }
+}
