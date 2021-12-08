ab05a3c0d1461771f0b92b94d214cf48425edf49
Юлия Ивановна Баранец <baranets.yi1251@oksei.onmicrosoft.com>
2021-12-08 23:00:58 +0500

    init

---
+++ pz_18(3)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pz_18_3_
+{
+    class Program
+    {
+        //Вариант 3
+        //Задание 3
+        static void Bomber(int a, int b)
+        {
+            Console.WriteLine(a);
+            if (a == b) return;
+            Bomber(b > a ? a + 1 : a - 1, b);
+        }
+        static void Main(string[] args)
+        {
+            Bomber(60, 0);
+        }
+    }
+}
