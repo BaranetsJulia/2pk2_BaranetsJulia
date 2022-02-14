4d80d0a49c07eabed4811a170edb6f0177f14a18
Юлия Ивановна Баранец <baranets.yi1251@oksei.onmicrosoft.com>
2022-02-14 22:09:27 +0500

    init

---
+++ pz_2s_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Companys;
+
+namespace pz_2s_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Company comp1 = new Company("Saturn");
+            comp1.GetCompanyInfo();
+            Company comp2 = new Company("Star", 13428151);
+            comp2.GetCompanyInfo();
+        }
+    }
+}
+
